2fd1edb498d0cc63e6b66886b944964d9a9b1f4e
Markel Mencia <markel.mnc@gmail.com>
2025-03-31 10:15:54 +0200

    Fix TypeImm constructor

--- src/instructions/TypeImm.java
+++ src/instructions/TypeImm.java
@@ -8,7 +8,7 @@ public class TypeImm extends Instruction {
 	private BitSet rs1;
 	private BitSet funct3;
 	private BitSet rd;
-	private int instr30;
+	private BitSet instr30;
 	
 	public BitSet getImm12() {
 		return imm12;
@@ -41,20 +41,20 @@ public class TypeImm extends Instruction {
 	public void setRd(BitSet rd) {
 		this.rd = rd;
 	}
-	
-	public int getInstr30() {
+
+	public BitSet getInstr30() {
 		return instr30;
 	}
-	
+
 	public void setInstr30(boolean instr30) {
 		if (instr30) {
-			this.instr30 = 1;
+			this.instr30.set(0);
 		} else {
-			this.instr30 = 0;
+			this.instr30.clear();
 		}
 	}
 
-	public TypeImm(BitSet instruction, BitSet imm12, BitSet rs1, BitSet funct3, BitSet rd, int instr30) {
+	public TypeImm(BitSet imm12, BitSet rs1, BitSet funct3, BitSet rd, BitSet instr30) {
 		super();
 		this.imm12 = imm12;
 		this.rs1 = rs1;
@@ -69,6 +69,6 @@ public class TypeImm extends Instruction {
 		this.rs1 = new BitSet(5);
 		this.funct3 = new BitSet(3);
 		this.rd = new BitSet(5);
-		this.instr30 = 1;
+		this.instr30 = new BitSet(1);
 	}
 }
