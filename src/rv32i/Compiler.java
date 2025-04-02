6894cddd471df2eb8fef0a8a09d520f3bf683161
Markel Mencia <markel.mnc@gmail.com>
2025-04-02 19:12:04 +0200

    Remove btiu(), btis() and bitExtension() from Compiler

--- src/rv32i/Compiler.java
+++ src/rv32i/Compiler.java
@@ -3,6 +3,7 @@ package rv32i;
 import instructions.*;
 import java.util.BitSet;
 import java.util.HashMap;
+import utils.Utils;
 
 /*	
 	csrrw: TODO
@@ -135,60 +136,6 @@ public class Compiler {
 		}	
 		return result;
 	}
-	
-	/**
-	 * Extends a bit array to a desired bit ammount.
-	 * @param segment The instruction array that will be filtered.
-	 * @param resultBits The desired bit length.
-	 * @return The extended BitSet.
-	 */
-	public static BitSet bitExtension(BitSet segment, int resultBits) {
-		BitSet result = new BitSet(resultBits);
-		
-		for (int i = 0; i < segment.length(); i++) {
-			result.set(i, segment.get(i));
-		}
-		return result;
-	}
-	
-	/**
-	 * Converts a binary array to a base-10 integer (unsigned)
-	 */
-	public static int btiu(BitSet segment) {
-		
-		// Binary to int unsigned.
-		
-		int result = 0;
-
-		for (int i = 0; i < segment.length(); i++) {
-			if (segment.get(i)) {
-				result |= (1 << i);
-			}
-		}
-		return result;
-	}
-	
-	/**
-	 * Converts a binary array to a base-10 integer (unsigned)
-	 */
-	public static int btis(BitSet segment, int size) {
-		
-		// Binary to int signed. Due to BitSize having
-		// a dinamic length depending on the last bit set,
-		// a size parameter is needed.
-
-		int result = 0;
-
-		for (int i = 0; i < size; i++) {
-			if (segment.get(i)) {
-				result |= (1 << i);
-			}
-		}
-		if (segment.get(size - 1)) {
-			result -= (1 << size);
-		}
-		return result;
-	}
 
 	/**
 	 * Gets the value of a register up to size
@@ -212,7 +159,7 @@ public class Compiler {
 			codeop.set(i, instr.get(i));
 		}
 
-		return codeopToInstr.get(btiu(codeop));
+		return codeopToInstr.get(Utils.btiu(codeop));
 	}
 	
 
