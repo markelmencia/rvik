81c971cd627308cf70b5c501e745b0c2a499a28d
Markel Mencía <markel.mnc@gmail.com>
2025-04-25 12:37:12 +0200

    Fix Menu Item class

--- src/gui/RvikMenuItem.java
+++ src/gui/RvikMenuItem.java
@@ -2,15 +2,14 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class RvikMenuItem extends JMenuItem {
     public RvikMenuItem(String name, int shortcut, int mnemonic) {
+        setText(name);
         setMnemonic(mnemonic);
         // 0 means no accelerator
         if (shortcut != 0) {
-            setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+            setAccelerator(KeyStroke.getKeyStroke(shortcut, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
         }
-        addActionListener(actionEvent -> System.out.println("New"));
     }
 }
